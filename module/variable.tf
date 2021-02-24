7c6257fa79fff05bb7df32a7678c81942e08e781
nabdilatipova <78001233+nabdilatipova@users.noreply.github.com>
2021-02-24 15:34:43 -0600

    Update variable.tf

--- module/variable.tf
+++ module/variable.tf
@@ -1,12 +1,16 @@
-variable "cidr_block" {}
+variable "instance_type" {}
 variable "region" {}
+variable "min_size" {}
+variable  "max_size" {}
+variable  "desired_capacity" {}
+variable "cidr_block" {}
 variable "private_cidr_block1" {}
 variable "private_cidr_block2" {}
 variable "private_cidr_block3" {}
 variable "public_cidr_block1" {}
 variable "public_cidr_block2" {}
 variable "public_cidr_block3" {}
-variable "instance_type" {}
+
 variable "ami_id" {}
 variable "ami_id_DB" {}
 
